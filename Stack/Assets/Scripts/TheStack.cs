20a10bcaa9a3095976d3834dd4e413e312669441
fr0m_1ns1d3 <he4to19@gmail.com>
2019-03-23 19:41:57 +0200

    Cut_Down
    Added cutting by using vectors + added gravity in the end

--- Stack/Assets/Scripts/TheStack.cs
+++ Stack/Assets/Scripts/TheStack.cs
@@ -6,14 +6,24 @@ public class TheStack : MonoBehaviour
 {
     private const float BOUNDS_SIZE = 3.5f;
     private const float STACK_MOVING_SPEED = 5.0f;
+    private const float ERROR_MARGIN = 0.1f;
+
     private GameObject[] theStack;
+    private Vector2 stackBounds = new Vector2(BOUNDS_SIZE, BOUNDS_SIZE);
+
     private int stackIndex;
     private int scoreCount = 0;
+    private int combo = 0;
+
     private float tileTransition = 0.0f;
     private float tileSpeed = 2.5f;
     private float secondaryPosition;
+
     private bool isMovingOnX = true;
+    private bool GameOver = false;
+
     private Vector3 desiredPosition;
+    private Vector3 LastTilePosition;
 
 
 
@@ -48,6 +58,9 @@ public class TheStack : MonoBehaviour
 
     private void MoveTile()
     {
+        if (GameOver)
+            return;
+
         tileTransition += Time.deltaTime * tileSpeed;
         if(isMovingOnX)
         theStack[stackIndex].transform.localPosition = new Vector3(Mathf.Sin(tileTransition) * BOUNDS_SIZE, scoreCount, secondaryPosition);
@@ -57,6 +70,7 @@ public class TheStack : MonoBehaviour
 
     private void SpawnTile()
     {
+        LastTilePosition = theStack[stackIndex].transform.localPosition;
         stackIndex--;
         if (stackIndex < 0)
             stackIndex = transform.childCount - 1;
@@ -67,6 +81,42 @@ public class TheStack : MonoBehaviour
     private bool PlaceTile()
     {
         Transform t = theStack[stackIndex].transform;
+
+        if (isMovingOnX)
+        {
+            float deltaX = LastTilePosition.x - t.position.x;
+            if(Mathf.Abs(deltaX) > ERROR_MARGIN)
+            {
+                //cut the tile 
+                combo = 0;
+                stackBounds.x -= Mathf.Abs(deltaX);
+                if (stackBounds.x <= 0)
+                    return false;
+
+                float middle = LastTilePosition.x + t.localPosition.x / 2;
+                t.localScale= new Vector3(stackBounds.x, 1, stackBounds.y);
+                t.localPosition = new Vector3(middle - (LastTilePosition.x / 2),scoreCount,LastTilePosition.z);
+
+            }
+        }
+        else
+        {
+            float deltaZ = LastTilePosition.z - t.position.z;
+            if (Mathf.Abs(deltaZ) > ERROR_MARGIN)
+            {
+                //cut the tile 
+                combo = 0;
+                stackBounds.y -= Mathf.Abs(deltaZ);
+                if (stackBounds.y <= 0)
+                    return false;
+
+                float middle = LastTilePosition.z + t.localPosition.z / 2;
+                t.localScale = new Vector3(stackBounds.x, 1, stackBounds.y);
+                t.localPosition = new Vector3(LastTilePosition.x / 2, scoreCount, middle- (LastTilePosition.z/2));
+
+            }
+        }
+
         secondaryPosition = (isMovingOnX)
             ? t.localPosition.x
             : t.localPosition.z;
@@ -76,6 +126,8 @@ public class TheStack : MonoBehaviour
 
     private void EndGame()
     {
-
+        Debug.Log("You Lose");
+        GameOver = true;
+        theStack[stackIndex].AddComponent<Rigidbody>();
     }
 }
